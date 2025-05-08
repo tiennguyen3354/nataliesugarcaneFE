fe3485708fd7922105c19ed7236f6803d29e4131
Tien Nguyen <nguyen.tien@student.greenriver.edu>
2025-05-07 21:03:49 -0700

    deleted all the files, organize the code into frontend, make it a react application so it can connect it with database through the backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# nataliesugarcaneFE
-A web application for a sugarcane beverage store where users can learn about the business and explore the full menu of drinks.
--- script.js
+++
@@ -1,8 +0,0 @@
-let burger = document.querySelector(".burger");
-let navList = document.querySelector(".navList");
-
-burger.addEventListener('click', ()=>{
-    burger.classList.toggle("active");
-    
-    navList.classList.toggle("menu");
-})
